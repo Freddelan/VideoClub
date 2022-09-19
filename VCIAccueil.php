65dff2b68fe8159128e9666aaa3f95162dc3b761
Freddelan <happyrush@hotmail.com>
2022-09-19 10:36:25 +0200

    modifications

--- VCIAccueil.php
+++ VCIAccueil.php
@@ -14,6 +14,16 @@ include("entete.php");
 <!-- 
 Coeur de la page 
 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 
 <div class="container-fluid">
     <?php
@@ -33,12 +43,66 @@ Coeur de la page
                 <h4 class="text-center">BIENVENUE SUR LE SITE DU VIDEO CLUB !</h4>
             </div>
             <div id="corps" class="text-center">
-                <img src="pictures/pictures/DesignVideoClub/Rideau.png" alt="Rideau cinéma" class="img-fluid w-100">
+                <img src="src/pictures/DesignVideoClub/BackGroundHCAc.jpg" alt="Rideau cinéma" class="img-fluid w-100">
             </div>
         </div>
     </div>
 </div>
+</body>
+</html>
+<style>
+  /* @keyframes tipsy {
+  0 {
+    transform: translateX(-50%) translateY(-50%) rotate(0deg);
+  }
+  100% {
+    transform: translateX(-50%) translateY(-50%) rotate(360deg);
+  }
+}
 
+body {
+  font-family: helvetica, arial, sans-serif;
+  background-color: #2e2e31;
+}
+
+a {
+  color: #fffbf1;
+  text-shadow: 0 20px 25px #2e2e31, 0 40px 60px #2e2e31;
+  font-size: 80px;
+  font-weight: bold;
+  text-decoration: none;
+  letter-spacing: -3px;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+
+a:before,
+a:after {
+  content: '';
+  padding: .9em .4em;
+  position: absolute;
+  left: 50%;
+  width: 100%;
+  top: 50%;
+  display: block;
+  border: 15px solid red;
+  transform: translateX(-50%) translateY(-50%) rotate(0deg);
+  animation: 10s infinite alternate ease-in-out tipsy;
+}
+
+a:before {
+  border-color: #d9524a #d9524a rgba(0, 0, 0, 0) rgba(0, 0, 0, 0);
+  z-index: -1;
+}
+
+a:after {
+  border-color: rgba(0, 0, 0, 0) rgba(0, 0, 0, 0) #d9524a #d9524a;
+  box-shadow: 25px 25px 25px rgba(46, 46, 49, .8);
+} 
+</style>
 
 <?php
 // J'inclue ici en fin de page la fin de mon code html donc je récupere l'intégralité du contenu de pied.php
